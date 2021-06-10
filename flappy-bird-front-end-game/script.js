153414630099d912fec5c5489aea3b41c9ce7d69
Shreya Verma <72500761+shreya-v7@users.noreply.github.com>
2021-06-10 22:38:33 +0530

    Update script.js

--- flappy-bird-front-end-game/script.js
+++ flappy-bird-front-end-game/script.js
@@ -1,7 +1,44 @@
-var block = document.getElementById("block");
-var hole = document.getElementById("hole");
+alert('Start Game')
 
-hole.addEventListener('animationiteration', () => {
-    var random = ((Math.random()*300) + 150);
-    hole.style.top = random + "px";
+var bars = document.getElementById("bars");
+var space = document.getElementById("space");
+var character = document.getElementById("character");
+var jumping = 0;
+var counter = 0;
+
+space.addEventListener('animationiteration', () => {
+    var random = -((Math.random()*300)+150);
+    space.style.top = random + "px";
+    counter++;
 });
+setInterval(function(){
+    var characterTop = parseInt(window.getComputedStyle(character).getPropertyValue("top"));
+    if(jumping==0){
+        character.style.top = (characterTop+3)+"px";
+    }
+    var barsLeft = parseInt(window.getComputedStyle(bars).getPropertyValue("left"));
+    var spaceTop = parseInt(window.getComputedStyle(space).getPropertyValue("top"));
+    var cTop = -(500-characterTop);
+    if((characterTop>480)||((barsLeft<20)&&(barsLeft>-50)&&((cTop<spaceTop)||(cTop>spaceTop+130)))){
+        alert("Uh Oh! Play Again! Score: "+(counter-1));
+        character.style.top = 100 + "px";
+        counter=0;
+    }
+},10);
+
+function jump(){
+    jumping = 1;
+    let jumpCount = 0;
+    var jumpInterval = setInterval(function(){
+        var characterTop = parseInt(window.getComputedStyle(character).getPropertyValue("top"));
+        if((characterTop>6)&&(jumpCount<15)){
+            character.style.top = (characterTop-5)+"px";
+        }
+        if(jumpCount>20){
+            clearInterval(jumpInterval);
+            jumping=0;
+            jumpCount=0;
+        }
+        jumpCount++;
+    },10);
+}
